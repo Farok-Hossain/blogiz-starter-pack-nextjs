1f9dc8873e0998750ca41c239936d17d0d70d476
Farok Hossain <farokafs7@gmail.com>
2024-07-02 22:57:31 +0600

    LatestBlogs updated

--- src/components/LatestBlogs/LatestBlogs.tsx
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -1,7 +1,15 @@
 const LatestBlogs = () => {
   return (
     <div>
-      <h1>Latest Blogs From Blogiz</h1>
+      <h1 className="text-center text-3xl my-5">
+        Latest Blogs From <span className=" text-accent">Blogiz</span>
+      </h1>
+      <p className="text-xl text-center text-gray-400 w-2/4 mx-auto">
+        <i>
+          UEFA Euro 2024: full football match schedule, start times, format and
+          teams
+        </i>
+      </p>
     </div>
   );
 };
