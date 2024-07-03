f645a4f90d964d49a70c5d7ac8373a80ff247ffa
Farok Hossain <farokafs7@gmail.com>
2024-07-03 23:15:06 +0600

    updated

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,10 +1,19 @@
 import BlogDetails from "@/components/ui/BlogDetails";
+import { Blog } from "@/types";
 
 interface BlogId {
   params: {
     blogId: string;
   };
 }
+
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
 
 const BlogDetailPage = async ({ params }: BlogId) => {
   // console.log(params);
